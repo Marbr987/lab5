0fb6f40b3b05f156779ad5068d29605a085ffff0
Marc Braun <marc.y.braun@live.de>
2021-09-28 10:24:06 +0200

    Finish vignette and description of shiny app

--- R/app.R
+++ R/app.R
@@ -1,7 +1,9 @@
-#' @export
-#' @descition A shiny application visualizing the position of two swedish cities and calculating their p-norm distance.
+#' @title Shiny app
+#' @name app
+#' @description A shiny application visualizing the position of two swedish cities and calculating their p-norm distance.
 #' @import shiny
 #' @importFrom ggplot2 ggplot geom_point geom_line geom_vline theme aes element_line element_rect labs
+#' @export
 
 library(shiny)
 
